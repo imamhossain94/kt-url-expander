14d20fbce7f94e08df53e124be2bde079c7997f5
imamhossain94 <imamagun94@gmail.com>
2021-11-08 22:49:43 +0600

    code complete

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,33 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.net.HttpURLConnection
+import java.net.URL
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    println(URLExpander.expand("https://tinyurl.com/8wa5w2o"))
 }
+
+
+object URLExpander {
+    fun expand(url: String): String {
+        var connection: HttpURLConnection
+        var finalUrl = url
+        try {
+            do {
+                connection = URL(finalUrl).openConnection() as HttpURLConnection
+                connection.instanceFollowRedirects = false
+                connection.useCaches = false
+                connection.requestMethod = "GET"
+                connection.connect()
+                val responseCode = connection.responseCode
+                if (responseCode in 300..399) {
+                    val redirectedUrl = connection.getHeaderField("Location") ?: break
+                    finalUrl = redirectedUrl
+                } else break
+            } while (connection.responseCode != HttpURLConnection.HTTP_OK)
+            connection.disconnect()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return finalUrl
+    }
+}
+
